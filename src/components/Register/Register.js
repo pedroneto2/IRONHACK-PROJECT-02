72a3d5db2188dbf247fc7df2a62e4b6b54eab277
carloshoa <carloshoarantes@gmail.com>
2021-10-02 10:51:07 -0300

    style validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Register.css";
 
 import axios from "axios";
@@ -13,11 +13,16 @@ import FormLabel from "@mui/material/FormLabel";
 import FormGroup from "@mui/material/FormGroup";
 import Switch from "@mui/material/Switch";
 import { useHistory } from "react-router";
+import { Typography } from "@mui/material";
 
 const INITIAL_CREDENTIALS = {
   name: "",
+  lastName: "",
   email: "",
+  cpf: null,
   password: "",
+  tel: null,
+  end: "",
   profileType: "clients",
 };
 
@@ -26,19 +31,73 @@ const handleChange = (e, credentials, setCredentials) => {
   setCredentials({ ...credentials, [name]: value });
 };
 
-const handleSubmit = (e, credentials, history) => {
+const handleSubmit = (
+  e,
+  credentials,
+  history,
+  error,
+  setBtnSubmit,
+  setMsgSubmit
+) => {
   e.preventDefault();
+  const emptyField = Object.values(credentials).some((elem) => !elem);
+  if (emptyField) {
+    setBtnSubmit("error");
+    setMsgSubmit("Campo Obrigatório não preenchido");
+    setTimeout(() => {
+      setBtnSubmit("primary");
+      setMsgSubmit("");
+    }, 2000);
+  } else {
+    console.log("arquivar");
+  }
 
-  axios
-    .post("https://ironrest.herokuapp.com/venere", credentials)
-    .then(() => {
-      history.push("/");
-    })
-    .catch((error) => console.log(error));
+  // console.log(Object.keys(credentials), Object.values(credentials));
+  // axios
+  //   .post("https://ironrest.herokuapp.com/venere", credentials)
+  //   .then(() => {
+  //     history.push("/");
+  //   })
+  //   .catch((error) => console.log(error));
+};
+
+const handleError = (e, error, setError, setErrorMsg) => {
+  const { name, value } = e.target;
+  console.log(" error ---> ", error);
+  if (value === "") {
+    setError({ ...error, [name]: true });
+    setErrorMsg("Campo Obrigatório");
+  } else {
+    setError(false);
+    setErrorMsg("");
+  }
 };
 
 const Register = () => {
   const [credentials, setCredentials] = useState({ ...INITIAL_CREDENTIALS });
+  const [error, setError] = useState({
+    name: false,
+    lastName: false,
+    cpf: false,
+    email: false,
+    password: false,
+    telephone: false,
+    address: false,
+    genre: false,
+  });
+  const [errorMsg, setErrorMsg] = useState({
+    name: "",
+    lastName: "",
+    cpf: "",
+    email: "",
+    password: "",
+    telephone: "",
+    address: "",
+    genre: "",
+  });
+
+  const [btnSubmit, setBtnSubmit] = useState("primary");
+  const [msgSubmit, setMsgSubmit] = useState("");
 
   const history = useHistory();
 
@@ -53,17 +112,47 @@ const Register = () => {
           type="text"
           value={credentials.name}
           onChange={(e) => handleChange(e, credentials, setCredentials)}
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.name}
+          helperText={errorMsg.name}
         />
-        <TextField fullWidth label="Sobrenome" variant="outlined" type="text" />
-        <TextField fullWidth label="CPF" variant="outlined" type="number" />
         <TextField
           fullWidth
+          name="lastName"
+          label="Sobrenome"
+          variant="outlined"
+          type="text"
+          onChange={(e) => handleChange(e, credentials, setCredentials)}
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.lastName}
+          helperText={errorMsg.lastName}
+        />
+        <TextField
+          fullWidth
+          name="cpf"
+          label="CPF"
+          variant="outlined"
+          type="number"
+          onChange={(e) =>
+            handleChange(e, credentials, setCredentials, setError, setErrorMsg)
+          }
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.cpf}
+          helperText={errorMsg.cpf}
+        />
+        <TextField
+          fullWidth
+          label="email"
           name="email"
-          label="Email (será o seu login)"
           variant="outlined"
           type="email"
           value={credentials.email}
-          onChange={(e) => handleChange(e, credentials, setCredentials)}
+          onChange={(e) =>
+            handleChange(e, credentials, setCredentials, setError, setErrorMsg)
+          }
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.email}
+          helperText={errorMsg.email}
         />
         <TextField
           fullWidth
@@ -72,13 +161,49 @@ const Register = () => {
           variant="outlined"
           type="password"
           value={credentials.password}
-          onChange={(e) => handleChange(e, credentials, setCredentials)}
+          onChange={(e) =>
+            handleChange(e, credentials, setCredentials, setError, setErrorMsg)
+          }
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.password}
+          helperText={errorMsg.password}
         />
-        <TextField fullWidth label="Telefone" variant="outlined" type="tel" />
-        <TextField fullWidth label="Endereço" variant="outlined" type="text" />
-        <FormControl component="fieldset">
+        <TextField
+          fullWidth
+          name="telephone"
+          label="Telefone"
+          variant="outlined"
+          type="tel"
+          onChange={(e) =>
+            handleChange(e, credentials, setCredentials, setError, setErrorMsg)
+          }
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.telephone}
+          helperText={errorMsg.telephone}
+        />
+        <TextField
+          fullWidth
+          name="address"
+          label="Endereço"
+          variant="outlined"
+          type="text"
+          onChange={(e) =>
+            handleChange(e, credentials, setCredentials, setError, setErrorMsg)
+          }
+          onBlur={(e) => handleError(e, error, setError, setErrorMsg)}
+          error={error.address}
+          helperText={errorMsg.address}
+        />
+        <FormControl
+          error={error.genre}
+          helperText={error.genre}
+          component="fieldset"
+        >
           <FormLabel component="legend">Genero</FormLabel>
-          <RadioGroup defaultValue="female" name="radio-buttons-group">
+          <RadioGroup
+            name="radio-buttons-group"
+            sx={{ display: "flex", flexDirection: "row" }}
+          >
             <FormControlLabel
               value="feminino"
               control={<Radio />}
@@ -89,6 +214,7 @@ const Register = () => {
               control={<Radio />}
               label="Masculino"
             />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
           </RadioGroup>
         </FormControl>
         <FormGroup>
@@ -107,11 +233,23 @@ const Register = () => {
           />
         </FormGroup>
         <Button
-          onClick={(e) => handleSubmit(e, credentials, history)}
+          onClick={(e) =>
+            handleSubmit(
+              e,
+              credentials,
+              history,
+              error,
+              setBtnSubmit,
+              setMsgSubmit
+            )
+          }
           variant="contained"
+          color={btnSubmit}
         >
           Register
         </Button>
+
+        <Typography sx={{ color: "red" }}> {msgSubmit}</Typography>
       </div>
     </div>
   );
