7985db08e0daa35a28a6f325294505cd0f926036
pedroneto2 <pedro.neto992@gmail.com>
2021-10-02 21:39:06 -0300

    CalendarScheduler component implemented

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-//import PrivateComponent from "./components/PrivateComponent/PrivateComponent"; //--> para teste
+import TesterComponent from "./components/TesterComponent/TesterComponent";
 
 import { Switch } from "react-router-dom";
 
@@ -11,8 +11,9 @@ import CustomRoute from "./utils/CustomRoute";
 function Routes() {
   return (
     <div className="App">
-      <NavBar linkList={["home", "link2"]}>
+      <NavBar linkList={["home", "test"]}>
         <Switch>
+          <CustomRoute exact path="/test" component={TesterComponent} />
           <CustomRoute exact path="/register" component={Register} />
           <CustomRoute exact path="/login" component={Login} />
           <CustomRoute exact path="/home" component={Home} />
---
+++ src/components/CalendarScheduler/CalendarScheduler.js
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from "react";
+import "./CalendarScheduler.css";
+
+import axios from "axios";
+
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+
+const retrieveUsers = (
+  clientID,
+  professionalID,
+  setClientUser,
+  setProfessionalUser,
+  setMaxDate
+) => {
+  axios
+    .get("https://ironrest.herokuapp.com/venere/" + clientID)
+    .then((response) => {
+      const { schedule } = response.data;
+      setClientUser({ schedule });
+    })
+    .catch((err) => console.log(err));
+
+  axios
+    .get("https://ironrest.herokuapp.com/venere/" + professionalID)
+    .then((response) => {
+      const {
+        maxDays,
+        notWorkingDays,
+        weekNotWorkingDays,
+        dayNotWorkingHours,
+        schedule,
+      } = response.data;
+      setProfessionalUser({
+        maxDays,
+        notWorkingDays,
+        weekNotWorkingDays,
+        dayNotWorkingHours,
+        schedule,
+      });
+      const maxDate = new Date();
+      maxDate.setDate(maxDate.getDate() + maxDays);
+      setMaxDate(maxDate);
+    })
+    .catch((err) => console.log(err));
+};
+
+const retrieveDisabledHours = (
+  currentDate,
+  setDisabledHours,
+  clientSchedule,
+  professionalSchedule,
+  disabledDays,
+  setDisabledDays
+) => {
+  const allDisabledHours = [];
+  const allDisabledDateHours = [...clientSchedule, ...professionalSchedule];
+  allDisabledDateHours.forEach((date) => {
+    if (new Date(date.date).getDate() === currentDate.getDate()) {
+      const disabledHour = new Date(date.date).getHours();
+      for (let i = 0; i < date.duration; i++) {
+        allDisabledHours.push(disabledHour + i);
+      }
+    }
+    !allDisabledHours.some((hour) => hour <= 24) &&
+      setDisabledDays([...disabledDays, date]);
+  });
+  setDisabledHours([...allDisabledHours]);
+};
+
+const CalendarSecheduler = ({ value, setValue, clientID, professionalID }) => {
+  const [maxDate, setMaxDate] = useState();
+  const [disabledDays, setDisabledDays] = useState([]);
+  const [disabledHours, setDisabledHours] = useState([]);
+
+  const [clientUser, setClientUser] = useState({
+    schedule: [],
+  });
+  const [professionalUser, setProfessionalUser] = useState({
+    maxDays: 0,
+    notWorkingDays: [],
+    weekNotWorkingDays: [],
+    dayNotWorkingHours: [],
+    schedule: [],
+  });
+
+  useEffect(() => {
+    retrieveUsers(
+      clientID,
+      professionalID,
+      setClientUser,
+      setProfessionalUser,
+      setMaxDate
+    );
+  }, []);
+
+  useEffect(() => {
+    retrieveDisabledHours(
+      value,
+      setDisabledHours,
+      clientUser.schedule,
+      professionalUser.schedule,
+      disabledDays,
+      setDisabledDays
+    );
+  }, [value]);
+
+  return (
+    <div className="calendar-scheduler-container">
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DateTimePicker
+          renderInput={(props) => (
+            <TextField
+              {...props}
+              inputProps={{
+                ...props.inputProps,
+                readOnly: true,
+              }}
+            />
+          )}
+          ampm={false}
+          disableHighlightToday
+          views={["day", "hours"]}
+          label="DateTimePicker"
+          value={value}
+          minDate={new Date()}
+          maxDate={maxDate}
+          shouldDisableTime={(hour) =>
+            disabledHours.some((disabledHour) => disabledHour === hour) ||
+            professionalUser.dayNotWorkingHours.some(
+              (disabledHour) => disabledHour === hour
+            )
+          }
+          shouldDisableDate={(date) =>
+            disabledDays.some(
+              (disabledDay) =>
+                new Date(disabledDay.date).getDate() === date.getDate()
+            ) ||
+            professionalUser.weekNotWorkingDays.some(
+              (disabledWeekDay) => disabledWeekDay === date.getDay()
+            ) ||
+            professionalUser.notWorkingDays.some(
+              (notWorkingDay) =>
+                new Date(notWorkingDay).getDate() === date.getDate()
+            )
+          }
+          onChange={(newValue) => {
+            newValue.setMinutes(0);
+            setValue(newValue);
+          }}
+        />
+      </LocalizationProvider>
+    </div>
+  );
+};
+
+export default CalendarSecheduler;
