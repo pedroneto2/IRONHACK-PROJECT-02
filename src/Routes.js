9d5b924e5e26fffcf3ccc07275e8a2f869b3f2b1
carloshoa <carloshoarantes@gmail.com>
2021-10-04 23:51:57 -0300

    update Profile on going

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
+import ProfessionalHome from "./components/Home/ProfessionalHome/ProfessionalHome";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 //import PrivateComponent from "./components/PrivateComponent/PrivateComponent"; //--> para teste
@@ -15,7 +16,7 @@ function Routes() {
         <Switch>
           <CustomRoute exact path="/register" component={Register} />
           <CustomRoute exact path="/login" component={Login} />
-          <CustomRoute exact path="/home" component={Home} />
+          <CustomRoute exact path="/home" component={ProfessionalHome} />
           <CustomRoute exact path="/" component={Home} />
         </Switch>
       </NavBar>
